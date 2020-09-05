405093bae2b836f9d5209b61918b92e2cc5be1ed
sdonohue0918 <sdonohue0918@gmail.com>
2020-09-05 14:45:24 -0400

    all rspec passing for GetRequester

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,33 @@
-# Write your code here
+require 'json'
+require 'net/http'
+require 'open-uri'
+
+
+
+
+class GetRequester
+
+    URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    
+    # JSON = self.parse_json
+
+    def initialize(url_string)
+        @url_string = url_string
+    end
+    
+    def get_response_body
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+
+    
+    def parse_json
+        response_parsed = JSON.parse(self.get_response_body)
+        response_parsed
+
+    end
+
+
+end
